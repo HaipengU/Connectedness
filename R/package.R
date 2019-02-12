d4de28141c2dcc55e4e2cc4eeb35a21961ab9f07
Haipeng Yu <haipeng.u@gmail.com>
2019-02-11 19:27:14 -0500

    update

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' Measurement of genetic connectedness 
+#'
+#' This is an R package to estimate the genetic connectedness across management units.
+#' @docType package
+#' @name Connectedness-package
+#' @aliases Connectedness
+#' @author HaipengYu <\url{https://haipengu.github.io/}>
+#' @seealso 
+#' 
+#' @references 
+NULL
