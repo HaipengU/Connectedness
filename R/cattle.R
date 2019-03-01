8f05765d17d362b2884c9f480827efe9cd33071c
Haipeng Yu <haipeng.u@gmail.com>
2019-03-01 11:45:11 -0500

    add dataset

---
+++ R/cattle.R
@@ -0,0 +1,21 @@
+#' Cattle dataset 
+#'
+#' The cattle dataset is simulated with QMSim software (Sargolzaei and Schenkel, 2009).   
+#' This dataset includes 530 individuals across five generations (from founder to generation 4), 
+#' each with 10, 000 single nucleotide polymorphisms across 29 autosomes. One phenotype with heritability of 0.6 was simulated.
+#' Three fixed covariates of sex, the management unit, and covariate 3 are available.
+#'
+#' @docType data
+#' @name cattle
+#' @usage data(cattle)
+#'
+#' @format 
+#' cattle.pheno contains phenotype, fixed covariates and pedigree information.
+#' catlle. X contains marker information.
+#' 
+#' @keywords datasets
+#'
+#' @references Sargolzaei, M., and F. S. Schenkel. 2009. Qmsim: a large-scale
+#' genome simulator for livestock. Bioinformatics 25:680–681. doi:10.1093/bioinformatics/btp045
+#'
+NULL
