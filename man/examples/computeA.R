bab453fc8f0a390c7cfcd705207f954cf1c25901
Haipeng Yu <haipeng.u@gmail.com>
2019-06-08 17:03:14 -0400

    update man

--- man/examples/computeA.R
+++ man/examples/computeA.R
@@ -8,5 +8,5 @@ data(GCcattle)
 head(cattle.pheno)
 
 # Construct numerator relationship matrix
-
-NRM <- computeA(Progeny = cattle.pheno$Progeny, Sire = cattle.pheno$Sire, Dam = cattle.pheno$Dam)
+NRM <- computeA(Progeny = cattle.pheno$Progeny, Sire = cattle.pheno$Sire, 
+		Dam = cattle.pheno$Dam)
